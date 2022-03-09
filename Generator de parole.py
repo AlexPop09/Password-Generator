a2bb87a189697fff3e3963ce011d528dc6fe3a77
AlexPop09 <100422784+AlexPop09@users.noreply.github.com>
2022-03-09 17:17:47 +0200

    Add files via upload

---
+++ Generator de parole.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 27 21:37:52 2022
+
+@author: alex
+"""
+import string
+import random
+
+
+## characters to generate password from
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()<>?|/+_-")
+while 1:
+    password_count=int(input("Enter number of passwords: "))
+    ## length of password from the user
+    password_len=int(input("Enter passwords length: "))
+    for i in range(0,password_count):
+        password=" "
+        for i in range(0,password_len):
+            password_characters=random.choice(characters)
+            password=password+password_characters
+            
+        print("Here is your random password: ",password)
+        
+        
